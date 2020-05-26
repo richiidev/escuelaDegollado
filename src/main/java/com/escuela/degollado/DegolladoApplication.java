1ed7af068fb94044c41dc61876973732d82cffbb
Ricardo Beltran <ricardo.beltran@sanmina.com>
2020-05-26 12:04:16 -0500

    proyecto Spring escuela Degollado

---
+++ src/main/java/com/escuela/degollado/DegolladoApplication.java
@@ -0,0 +1,13 @@
+package com.escuela.degollado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegolladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegolladoApplication.class, args);
+	}
+
+}
